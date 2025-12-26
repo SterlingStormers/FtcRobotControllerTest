21b46e387d4bfd4941e2f297663fca428482f8a9
helotyr <psahaj875@gmail.com>
2025-12-26 13:37:05 -0500

    stuf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBottom9Blue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBottom9Blue.java
@@ -168,8 +168,7 @@ public class AutoBottom9Blue extends OpMode {
             case 0:
                 drive.intakeMotor.setPower(0);
                 drive.shooterMotor.setPower(0);
-                drive.kicker.setDirection(0);
-                drive.hood.setDirection(0);
+
                 setPathState(1);
                 break;
 
@@ -325,8 +324,6 @@ public class AutoBottom9Blue extends OpMode {
                     drive.intakeMotor.setPower(0);
                     drive.shooterMotor.setPower(0);
                     drive.spindexer.setPower(0);
-                    drive.kicker.setDirection(0);
-                    drive.hood.setDirection(0);
                     //To be changed
                     pathState = -1;
                 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTop12Blue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTop12Blue.java
@@ -202,8 +202,6 @@ public class AutoTop12Blue extends OpMode {
             case 0:
                 drive.intakeMotor.setPower(0);
                 drive.shooterMotor.setPower(0);
-                drive.kicker.setDirection(0);
-                drive.hood.setDirection(0);
                 setPathState(1);
                 break;
 
@@ -406,8 +404,6 @@ public class AutoTop12Blue extends OpMode {
                     drive.intakeMotor.setPower(0);
                     drive.shooterMotor.setPower(0);
                     drive.spindexer.setPower(0);
-                    drive.kicker.setDirection(0);
-                    drive.hood.setDirection(0);
                     //To be changed
                     pathState = -1;
                 }
