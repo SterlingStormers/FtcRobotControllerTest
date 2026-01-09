3ea0150d60a3d735a7ec70331db6de877c328fd0
helotyr <psahaj875@gmail.com>
2026-01-09 18:15:52 -0500

    Update TestAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
@@ -16,116 +16,15 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 @Configurable // Panels
 public class TestAuto extends OpMode {
 
-    private TelemetryManager panelsTelemetry; // Panels Telemetry instance
-    public Follower follower; // Pedro Pathing follower instance
-    private int pathState; // Current autonomous path state (state machine)
-    private Paths paths; // Paths defined in the Paths class
-    private DriveTrainHardware drive;
-    private Timer pathTimer, opmodeTimer;
 
     @Override
     public void init() {
-        panelsTelemetry = PanelsTelemetry.INSTANCE.getTelemetry();
 
-        follower = Constants.createFollower(hardwareMap);
-        follower.setStartingPose(new Pose(72, 8, Math.toRadians(90)));
-
-        paths = new Paths(follower); // Build paths
-
-        panelsTelemetry.debug("Status", "Initialized");
-        panelsTelemetry.update(telemetry);
-        drive = new DriveTrainHardware();
-        drive.init(hardwareMap);
-        pathTimer = new Timer();
-        opmodeTimer = new Timer();
     }
 
     @Override
     public void loop() {
-        follower.update(); // Update Pedro Pathing
-        pathState = autonomousPathUpdate(); // Update autonomous state machine
 
-        // Log values to Panels and Driver Station
-        panelsTelemetry.debug("Path State", pathState);
-        panelsTelemetry.debug("X", follower.getPose().getX());
-        panelsTelemetry.debug("Y", follower.getPose().getY());
-        panelsTelemetry.debug("Heading", follower.getPose().getHeading());
-        panelsTelemetry.update(telemetry);
-    }
-
-    public static class Paths {
-
-        public PathChain Path1;
-        public PathChain Path2;
-
-        public Paths(Follower follower) {
-            Path1 = follower
-                    .pathBuilder()
-                    .addPath(
-                            new BezierLine(new Pose(56.000, 8.000), new Pose(56.000, 30.000))
-                    )
-                    .setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(90))
-                    .build();
-
-            Path2 = follower
-                    .pathBuilder()
-                    .addPath(
-                            new BezierLine(new Pose(56.000, 30.000), new Pose(36.000, 30.000))
-                    )
-                    .setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(180))
-                    .build();
-        }
-    }
-
-    public void setPathState(int newState) {
-        pathState = newState;
-        pathTimer.resetTimer();
-    }
-
-    public int autonomousPathUpdate() {
-        // Add your state machine Here
-        // Access paths with paths.pathName
-        // Refer to the Pedro Pathing Docs (Auto Example) for an example state machine
-        double t = follower.getCurrentTValue();
-        follower.setMaxPower(1);
-
-        switch (pathState) {
-            case 0:
-                drive.intakeMotor.setPower(0);
-                drive.shooterMotor.setPower(0);
-                setPathState(1);
-                break;
-
-            case 1:
-                if (!follower.isBusy()) {
-                    follower.followPath(paths.Path1, true);
-                }
-                follower.update();
-                if (!follower.isBusy()) {
-                    setPathState(2);
-                }
-                break;
-
-            case 2:
-                //Tell HuskyLens to scan AprilTag
-                // HuskyLens will then send color pattern to ColorSensingAuto class for logic and this class for telemetry
-                // Do not block loop, use HuskyLens polling
-                setPathState(3);
-                break;
-            case 3:
-                if (!follower.isBusy()) {
-                    follower.followPath(paths.Path2, true);
-                }
-                follower.update();
-                //Color Sensing
-                //Spindexer logic
-                //Do not block loop
-                if (!follower.isBusy()) {
-                    setPathState(-1);
-                }
-                break;
-        }
-        return pathState;
     }
 }
 
