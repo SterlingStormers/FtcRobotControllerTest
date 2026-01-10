aec60115f40bbfca6fd0a8963db84e4f2b8a196f
helotyr <psahaj875@gmail.com>
2026-01-09 20:24:07 -0500

    Update TestAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import static org.firstinspires.ftc.teamcode.Tuning.follower;
+
 import com.bylazar.configurables.annotations.Configurable;
 import com.bylazar.telemetry.PanelsTelemetry;
 import com.bylazar.telemetry.TelemetryManager;
@@ -15,17 +17,57 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 @Autonomous(name = "Test Auto", group = "Autonomous")
 @Configurable // Panels
 public class TestAuto extends OpMode {
+    private DriveTrainHardware drive;
+    private Timer pathTimer, opmodeTimer;
+    private int pathState;
 
 
     @Override
     public void init() {
+        char ball1 = 'P';
+        char ball2 = 'G';
+        char ball3 = 'P';
+        char detectedBall1 = 'P';
+        char detectedBall2 = 'P';
+        char detectedBall3 = 'G';
+        char pos0 = detectedBall1;
+        char pos1 = detectedBall2;
+        char pos2 = detectedBall3;
+        drive = new DriveTrainHardware();
+        drive.init(hardwareMap);
+        pathTimer = new Timer();
+        opmodeTimer = new Timer();
 
     }
 
     @Override
     public void loop() {
-
+        pathState = autonomousPathUpdate();
     }
+        public void setPathState(int newState) {
+            pathState = newState;
+            pathTimer.resetTimer();
+        }
+        public int autonomousPathUpdate() {
+        switch(pathState) {
+            case 0:
+                //Set spindexer to 90
+                drive.intakeMotor.setPower(1);
+                setPathState(1);
+                break;
+            case 1:
+                if (pathTimer.seconds() > 2.0)
+
+
+        }
+        return pathState;
+        }
+
+
+
+
+
+
 }
 
 
