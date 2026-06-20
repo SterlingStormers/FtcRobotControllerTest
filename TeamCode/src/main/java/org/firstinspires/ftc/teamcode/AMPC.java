13d6f9e29a89ededd5ce23c1a53808af071fad31
helotyr <psahaj875@gmail.com>
2026-06-19 20:56:48 -0400

    Update AMPC.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AMPC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AMPC.java
@@ -195,9 +195,9 @@ public class AMPC {
         double cosH = Math.cos(heading);
         double sinH = Math.sin(heading);
         //Forward speed scrubbing based on turning
-        double scrubbingLossMultiplier = 1.0 - (Math.abs(omega) / maxTurnRateRad) * SCRUBBING_FACTOR;
-        double fieldVx = (vx * cosH) - (vy * sinH) * scrubbingLossMultiplier;
-        double fieldVy = (vx * sinH) + (vy * cosH) * scrubbingLossMultiplier;
+//        double scrubbingLossMultiplier = 1.0 - (Math.abs(omega) / maxTurnRateRad) * SCRUBBING_FACTOR;
+        double fieldVx = (vx * cosH) - (vy * sinH) /* * scrubbingLossMultiplier*/;
+        double fieldVy = (vx * sinH) + (vy * cosH) /* * scrubbingLossMultiplier*/;
 
         double predictedX = robotPose.getX() + (fieldVx * HORIZON_SECONDS);
         double predictedY = robotPose.getY() + (fieldVy * HORIZON_SECONDS);
@@ -222,7 +222,7 @@ public class AMPC {
         double dOmega = omega - lastBestOmega;
         double smoothnessCost = (WEIGHT_SMOOTHNESS_VX * (dVx * dVx)) + (WEIGHT_SMOOTHNESS_VY * (dVy * dVy)) + (WEIGHT_SMOOTHNESS_OMEGA * (dOmega * dOmega));
         //Dynamic endpoint cost
-        double endpointWeightMultiplier = 1.0 + (currentT * currentT * 4.0);
+//        double endpointWeightMultiplier = 1.0 + (currentT * currentT * 4.0);
         // Terminal cost: penalize "can't stop in time before path end"
         double speed = Math.sqrt((vx * vx) + (vy * vy));
         double brakeDist = (speed * speed) / (2.0 * MAX_DECEL);   // physics: v^2 = 2·a·d
@@ -234,12 +234,12 @@ public class AMPC {
         terminalTriggered = false;
 
         if (brakeDist > remainingDist) {
-            terminalCost = WEIGHT_TERMINAL * (brakeDist - remainingDist) * endpointWeightMultiplier;
+            terminalCost = WEIGHT_TERMINAL * (brakeDist - remainingDist) /* * endpointWeightMultiplier*/;
             terminalTriggered = true;
 
         }
 
-        return ((WEIGHT_LOOKAHEAD * distError * endpointWeightMultiplier) + (WEIGHT_PATH * distPathError * endpointWeightMultiplier) + (WEIGHT_HEADING * headingError) + smoothnessCost + terminalCost);
+        return ((WEIGHT_LOOKAHEAD * distError /* * endpointWeightMultiplier*/) + (WEIGHT_PATH * distPathError /* * endpointWeightMultiplier*/) + (WEIGHT_HEADING * headingError) + smoothnessCost + terminalCost);
 
     }
     private void computePurePursuit(Pose robotPose) {
