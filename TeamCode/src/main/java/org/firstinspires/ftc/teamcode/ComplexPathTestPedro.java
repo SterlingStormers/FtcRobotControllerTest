2f6ff8c7e3456bbb78eb350e1bc25c6eff6416bb
helotyr <psahaj875@gmail.com>
2026-07-05 21:28:35 -0400

    Update ComplexPathTestPedro.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ComplexPathTestPedro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ComplexPathTestPedro.java
@@ -85,6 +85,8 @@ public class ComplexPathTestPedro extends OpMode {
         drive.husky.selectAlgorithm(HuskyLens.Algorithm.TAG_RECOGNITION);
         drive.light.setPosition(1);
 
+        Drawing.init();
+
         try { Thread.sleep(500); } catch (InterruptedException ignored) {}
         follower.updatePose();
 
@@ -98,6 +100,12 @@ public class ComplexPathTestPedro extends OpMode {
 
         pathState = autonomousPathUpdate();
         colorScanner.update();
+
+        // Draw the path being followed + robot pose in Panels
+        Drawing.drawPath(paths.Path1, new com.bylazar.field.Style("", "#00FF00", 1));   // green path
+        Drawing.drawPoseHistory(follower.getPoseHistory());
+        Drawing.drawRobot(follower.getPose());
+        Drawing.sendPacket();
 
         if (ShooterSpinup && !follower.isBusy() && follower.getCurrentTValue() >= 0.25) {
             drive.shooterMotor.setPower(0.75);
